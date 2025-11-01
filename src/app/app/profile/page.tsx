7b8b17ee341f632dce8d2435ed5293985ba5a5a3
SUMIT PANDEY <145885347+sumitpandey-cell@users.noreply.github.com>
2025-11-01 20:13:34 +0530

    vercel error fix

--- src/app/app/profile/page.tsx
+++ src/app/app/profile/page.tsx
@@ -19,7 +19,7 @@ export default async function ProfilePage() {
 
   const userInitials = userName
     .split(" ")
-    .map((n) => n[0])
+    .map((n: string) => n[0])
     .join("")
     .toUpperCase()
     .slice(0, 2)
